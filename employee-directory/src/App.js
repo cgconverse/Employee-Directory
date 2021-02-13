545cd52d5250c3b1df392a1f50f31369d0e72fa7
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-13 00:32:32 -0800

    added custom hook

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -4,3 +4,9 @@ import User from "./components/User";
 import API from "./utils/API";
 import Sort from "./components/Sort"
 import Table from "./components/Table"
+
+// Custom Hook
+const useAsync = (asyncFunction, immediate = true) => {
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
