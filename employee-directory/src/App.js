1dcc1c3216bfe376c9616c60f73dc380bbe841a1
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-13 00:34:51 -0800

    updated index.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -10,3 +10,44 @@ const useAsync = (asyncFunction, immediate = true) => {
   const [loading, setLoading] = useState(false);
   const [value, setValue] = useState(null);
   const [error, setError] = useState(null);
+
+  const execute = useCallback(() => {
+    setLoading(true);
+    setValue(null);
+    setError(null);
+
+    return asyncFunction()
+      .then((response) => {
+        setValue(response);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+
+  return { execute, loading, value, error };
+};
+
+function App() {
+  const { loading, value = {data:{results:[]}}, error } = useAsync(API.getUsers);
+  if (loading || !value) return "loading..."; 
+  console.log(value);
+  return (
+    <div>
+      <Header />
+      {/* <Sort /> */}
+      <User />   
+    </div>
+  );
+}
+
+export default App;
